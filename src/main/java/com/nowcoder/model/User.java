3e7fa6f5ce9ffda7adca8dec47009951fd4ebb2b
whitetower <1065635703@qq.com>
2018-08-29 00:35:08 +0800

    数据库sql文件，pom.xml添加mysql相关包

--- src/main/java/com/nowcoder/model/User.java
+++ src/main/java/com/nowcoder/model/User.java
@@ -1,9 +1,20 @@
 package com.nowcoder.model;
 
 public class User {
+    private int id;
     private String name;
-    public User(String name){
+    private String password;
+    private String salt;
+    private String headUrl;
+
+    public User() {
+
+    }
+    public User(String name) {
         this.name = name;
+        this.password = "";
+        this.salt = "";
+        this.headUrl = "";
     }
 
     public String getName() {
@@ -12,5 +23,37 @@ public class User {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    public String getHeadUrl() {
+        return headUrl;
+    }
+
+    public void setHeadUrl(String headUrl) {
+        this.headUrl = headUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
