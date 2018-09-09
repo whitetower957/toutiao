605f950b805cffbc6ab1611ffcfacf128d1a5deb
whitetower <1065635703@qq.com>
2018-09-09 18:46:21 +0800

    评论中心

---
+++ src/main/java/com/nowcoder/service/CommentService.java
@@ -0,0 +1,30 @@
+package com.nowcoder.service;
+
+import com.nowcoder.dao.CommentDao;
+import com.nowcoder.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentDao commentDao;
+
+    public List<Comment> getCommentByEntity(int entityId,int entityType){
+        return commentDao.selectByEntity(entityId,entityType);
+    }
+
+    public int addComment(Comment comment){
+        return commentDao.addComment(comment);
+    }
+
+    public int getCommentCount(int entityId,int entityType){
+        return commentDao.getCommentCount(entityId,entityType);
+    }
+
+    public void deleteComment(int entityId,int entityType){
+        commentDao.updateStatus(entityId,entityType,1);
+    }
+}
